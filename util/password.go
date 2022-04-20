b2268cef78fb1c6c967120dbe29015b0bb799ee8
Andreis3 <andrei.as3@hotmail.com>
2022-04-20 08:56:02 -0300

    implements hash password

---
+++ util/password.go
@@ -0,0 +1,25 @@
+package util
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword returns the bcrypt hash of the password
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+	return string(hashedPassword), nil
+}
+
+// CheckPassword checks if the provider password is correct or not
+func CheckPassword(password, hashedPassword string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		return fmt.Errorf("failed to check password: %w", err)
+	}
+	return nil
+}
